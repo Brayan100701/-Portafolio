f24fd81a79a0b0cba362f4d72a040c22c32f8411
Brayan Sanchez <bsanchez100701@gmail.com>
2026-01-09 16:10:24 -0600

    Acerca-de: Responsive | cambio de colores | CV descargable

--- src/components/AcercaDe.tsx
+++ src/components/AcercaDe.tsx
@@ -3,39 +3,57 @@ export default function AcercaDe() {
     <>
       <section id="acerca-de" className="text-light bg-dark">
         <section id="contacto" className="container p-3 mb-2 text-light">
-          <article>
-            <p className="descripcion" id="hola">
-              ¡Hola!
-            </p>
-            <p className="descripcion" id="nombre">
-              Mi nombre es Brayan Benjamín Sánchez Guillén
-            </p>
-            <p className="descripcion" id="inge">
-              Ingeniero en Computación
-            </p>
-            <img
-              className="rounded-circle"
-              src="/perfilFormal.png"
-              id="perfil"
-            />
-            <div>
-              <a
-                target="_blank"
-                href="https://www.linkedin.com/in/brayan-benjamín-sánchez-guillén-362b1a3a2"
-              >
-                <img
-                  className="rounded-circle iconos-portada"
-                  src="/linkedIn.png"
-                />
-              </a>
-              <a target="_blank" href="https://github.com/Brayan100701">
-                <img
-                  className="rounded-circle iconos-portada"
-                  src="/tecnologias/github150.png"
-                />
-              </a>
+          <div className="row"></div>
+          <div className="row">
+            <div className="col">
+              <p className="descripcion" id="hola">
+                ¡Hola!
+              </p>
+              <p className="descripcion" id="nombre">
+                Mi nombre es Brayan Benjamín Sánchez Guillén
+              </p>
+              <p className="descripcion" id="inge">
+                Ingeniero en Computación
+              </p>
             </div>
-          </article>
+            <div className="col">
+              <img
+                className="rounded-circle"
+                src="/perfilFormal.png"
+                id="perfil"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <p> </p>
+          </div>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/brayan-benjamín-sánchez-guillén-362b1a3a2"
+          >
+            <img
+              className="rounded-circle iconos-portada"
+              src="/linkedIn.png"
+            />
+          </a>
+          <a target="_blank" href="https://github.com/Brayan100701">
+            <img
+              className="rounded-circle iconos-portada"
+              src="/tecnologias/github150.png"
+            />
+          </a>
+          <div className="row">
+            <a
+              className="btn btn-dark"
+              id="btnpdf"
+              href="/CV/CV-Brayan-Sanchez.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              download="CV-Brayan Sanchez.pdf"
+            >
+              Descarga mi CV
+            </a>
+          </div>
         </section>
       </section>
     </>
