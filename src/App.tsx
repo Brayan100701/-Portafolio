a9bc803bd2e40dfc9f9c0dd7432aa8f3a52d33bb
Brayan Sanchez <bsanchez100701@gmail.com>
2026-01-09 17:04:44 -0600

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import Welcome from "./components/Welcome";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Welcome />
